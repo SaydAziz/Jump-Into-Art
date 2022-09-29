e79754b0d2987a358f095c5627b60ed47409ad77
Aziz Saydaminov <sayd.aziz02@gmail.com>
2022-09-29 18:28:13 -0500

    Adds custom Scene Managment system (bug check needed).

---
+++ Assets/Project Source/Scene Managment/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Waypoint : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "VRAvatar") //might not need this check if nothing else has an RB
+        {
+            Debug.Log("Collided: " + other.gameObject.name);
+            SceneManager.Instance.CycleNextScene();
+        }
+        
+    }
+}
