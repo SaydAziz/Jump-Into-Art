c5bb24d6aa0fbde1ffdf08be999c6dfcd9a852bf
Aziz Saydaminov <sayd.aziz02@gmail.com>
2022-09-30 14:42:12 -0500

    Imports first graybox of map, and creats waypoint prefab.

--- Assets/Project Source/Scene Managment/Waypoint.cs
+++ Assets/Project Source/Scene Managment/Waypoint.cs
@@ -7,8 +7,8 @@ public class Waypoint : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "VRAvatar") //might not need this check if nothing else has an RB
-        {
+        if (other.gameObject.name == "VRAvatar") //might not need this check if nothing else has an RB.
+        {                                        //RB also seems to not keep the isKinematic setting when pulling through the git repo so make sure to turn that on.
             Debug.Log("Collided: " + other.gameObject.name);
             SceneManager.Instance.CycleNextScene();
         }
