ef760c32cb55cdb93573860abce8118cd98ad5ba
Aziz Saydaminov <sayd.aziz02@gmail.com>
2022-10-12 17:39:47 -0500

    Improves waypoints, and cleans up scene heirarchy.

--- Assets/Project Source/Scene Managment/Waypoint.cs
+++ Assets/Project Source/Scene Managment/Waypoint.cs
@@ -5,13 +5,21 @@ using Valve.VR.InteractionSystem;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] bool isScenePoint;
+    [SerializeField] bool isEventPoint;
+
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "VRAvatar") //might not need this check if nothing else has an RB.
-        {                                        //RB also seems to not keep the isKinematic setting when pulling through the git repo so make sure to turn that on.
-            Debug.Log("Collided: " + other.gameObject.name);
-            SceneManager.Instance.CycleNextScene();
+        if (isScenePoint)
+        {
+            if (other.gameObject.name == "VRAvatar") //might not need this check if nothing else has an RB.
+            {                                        //RB also seems to not keep the isKinematic setting when pulling through the git repo so make sure to turn that on.
+                Debug.Log("Collided: " + other.gameObject.name);
+                SceneManager.Instance.CycleNextScene();
+            }
         }
+        
         
     }
 }
