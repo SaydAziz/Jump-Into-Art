d7349fc84c946acfc9be9308912b56451ea35b67
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2022-10-16 01:13:01 -0500

    Script for Animation
    Created a new script file for the animation of the stalactite falling. This script will be used to layer multiple animations of objects falling. Experiencing issue where the animation needs to be set to legacy and have been unable to locate the rig tab to do so.

---
+++ Assets/Project Source/Events/StalactiteAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteAnim : MonoBehaviour
+{
+    private Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim["TestOneFallV.5"].layer = 123;
+    }
+
+    //double the fall speed when true
+    private bool fastFall = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        //Leave first stalactite to fall to complete before changing
+        if(anim.isPlaying)
+        {
+            return;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Test One Falling");
+            anim.Play("TestOneFallV.5");
+        }
+    }
+}
