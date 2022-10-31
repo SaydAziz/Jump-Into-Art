b3bcfb3831016a3b628edf120dfc4f7579476c62
unsc43 <74630988+unsc43@users.noreply.github.com>
2022-10-31 01:05:33 -0500

    Whale 2 (pt2)
    Trigger added to start animation event (whale 2)
    
    Whale animation improved

---
+++ Assets/Project Source/Events/EventScripts/WhaleEventSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleEventSpawn : MonoBehaviour
+{
+    //Trigger Event
+
+    //Variable to Hold Animation Controller
+    [SerializeField] private Animator MyAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Changes boolean to true to start animation
+            MyAnimationController.SetBool("PlayWhale", true);
+        }
+       
+    }
+
+
+
+
+}
