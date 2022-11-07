9d6cee98c5c4765387cb4989dd72246800405fdf
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2022-11-07 15:22:34 -0600

    Revert "Organize Hierarchy and Animation Folders"
    This reverts commit b95418efc126678a2d0f44bd1cb8e2bb0438d93d.

---
+++ Assets/Project Source/Events/EventScripts/EventManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManagement : MonoBehaviour
+{
+    // The time that the event is delayed
+    //float delayTime;
+
+    //  The time it takes for the event to take place
+    //float eventTime;
+
+    // The total of the event time and the delay time.
+    //float totalTime;
+
+    //bool pauseMovement = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
