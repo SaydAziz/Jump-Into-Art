cef6240e4f3aa9b6b5d1301b13987fc472c95165
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2022-11-07 11:42:17 -0600

    Stalactite Event - Animations play At Waypoint 16
    Stalactite Event functional. There are 5 groups of stalactites and separate animations for five of them. The animations plays once when the player reaches waypoint 16.
    
    To be added:
    - Speed alterations (Stalactites fall very fast, this needs to be slowed down)
    - Animation offset (Offset animation times so there is a delay between each animation start).

--- Assets/Project Source/Events/EventScripts/StalactiteAnim.cs
+++ Assets/Project Source/Events/EventScripts/StalactiteAnim.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class StalactiteAnim : MonoBehaviour
 {
-    [SerializeField] private Animator MyAnimationController;
+    [SerializeField] private Animator AnimationControllerOne, AnimationControllerTwo, AnimationControllerThree, AnimationControllerFour, AnimationControllerFive;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             //Changes boolean to true to start animation
-            MyAnimationController.SetBool("S_AnimOne", true);
-            MyAnimationController.SetBool("S_AnimTwo", true);
-            MyAnimationController.SetBool("S_AnimThree", true);
-            MyAnimationController.SetBool("S_AnimFour", true);
-            MyAnimationController.SetBool("S_AnimFive", true);
+            AnimationControllerOne.SetBool("PlayStalactite", true);
+            AnimationControllerTwo.SetBool("PlayStalactite", true);
+            AnimationControllerThree.SetBool("PlayStalactite", true);
+            AnimationControllerFour.SetBool("PlayStalactite", true);
+            AnimationControllerFive.SetBool("PlayStalactite", true);
         }
     }
 }
