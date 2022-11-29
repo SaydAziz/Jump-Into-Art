93bfa8fe785bc747d631eda17a78500a0f31964d
marpl6967 <72100598+marpl6967@users.noreply.github.com>
2022-11-29 11:42:35 -0600

    Added ore props to cave, have begun the process, tweaked anims for iso, made script for geyser not working yet

---
+++ Assets/Project Source/Scripts/TriggerGeyser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGeyser : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem Geyser;
+    [SerializeField] private AudioSource clip;
+    [SerializeField] private int soundDelay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Geyser.Play(Geyser);
+        SoundManager.Instance.PlaySound(clip, soundDelay);
+    }
+}
