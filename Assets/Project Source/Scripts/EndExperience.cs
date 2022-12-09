c9ce468066756d8213ca999b2321dd8ed85be79b
unsc43 <74630988+unsc43@users.noreply.github.com>
2022-12-08 18:45:34 -0600

    Fade to black + start effects added
    Added a probable fade to black (Potential placeholder - 2 be seen) and animations added.

---
+++ Assets/Project Source/Scripts/EndExperience.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.Core;
+using Liminal.Core.Fader;
+public class EndExperience : MonoBehaviour
+{
+
+    private int fadeDuration = 10;
+
+    private void OnTriggerEnter(Collider other)
+    { 
+    
+        var fader = ScreenFader.Instance;
+        fader.FadeTo(Color.black, fadeDuration);
+
+       // ExperienceApp.End();
+    }
+}
